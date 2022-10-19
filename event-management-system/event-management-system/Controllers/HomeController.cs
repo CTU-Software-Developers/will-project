4ad19ce1b412c6cf598982b7b19c23d8e2b8e38a
gituser334 <110392377+gituser334@users.noreply.github.com>
2022-10-19 11:55:22 +0200

    Reupload

---
+++ event-management-system/event-management-system/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace event_management_system.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult SportsReviews()
+        {
+            ViewBag.Message = "Your Sports Review page.";
+
+            return View();
+        }
+        public ActionResult MotorSportsReviews()
+        {
+            ViewBag.Message = "Your Motor sports Review page.";
+
+            return View();
+        }
+        public ActionResult RageExpoReviews()
+        {
+            ViewBag.Message = "Your Rage expo Review page.";
+
+            return View();
+        }
+    }
+}
---
+++ event-management-system/event-management-system/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(event_management_system.Startup))]
+namespace event_management_system
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
