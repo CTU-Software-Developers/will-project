ec54b2e6e8e065b3e052d2c9de42b5be788e4772
chris <lauren.fabian2001@gmail.com>
2022-10-24 10:26:57 +0200

    adding pages and data
    Adding razor pages for sports events and its history, the color theme remains the same.
    Added ActionResult functions to the home controller so newly created razor pages can display on the browser windows
    CSS homepage navbar vertical alignment bug is being addressed

--- event-management-system/event-management-system/Controllers/HomeController.cs
+++ event-management-system/event-management-system/Controllers/HomeController.cs
@@ -15,33 +15,39 @@ namespace event_management_system.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
         public ActionResult SportsReviews()
         {
-            ViewBag.Message = "Your Sports Review page.";
-
             return View();
         }
         public ActionResult MotorSportsReviews()
         {
-            ViewBag.Message = "Your Motor sports Review page.";
-
             return View();
         }
         public ActionResult RageExpoReviews()
         {
-            ViewBag.Message = "Your Rage expo Review page.";
-
+            return View();
+        }
+        public ActionResult Softball()
+        {
+            return View();
+        }
+        public ActionResult Baseball()
+        {
+            return View();
+        }
+        public ActionResult Cricket()
+        {
+            return View();
+        }
+        public ActionResult Hockey()
+        {
             return View();
         }
     }
